52dceac9cb8ac131d2f4fc26a29a6de9a93e51cd
alper ademoglu <alperademoglu@hotmail.com>
2022-09-16 23:22:14 -0400

    added date and time picker views

--- reservations/urls.py
+++ reservations/urls.py
@@ -4,16 +4,21 @@ from reservations.views import (
     ReservationListView,
     ReservationCreateView,
     ReservationDetailView,
-    ReservationUpdateView,
-    ReservationPickDateTimeView,
-    ReservationDeleteView
+    ReservationDeleteView,
+    ReservationDateUpdateView,
+    ReservationPickDateView,
+    ReservationTimeUpdateView,
+    ReservationPickTimeView,
 )
 
 urlpatterns = [
     path("", ReservationListView, name="home"),
     path("create/", ReservationCreateView, name="create_reservation"),
-    path("<int:pk>/datetime", ReservationPickDateTimeView, name="datetime"),
-    path("<int:pk>/updated/", ReservationUpdateView, name="updated"),
     path("<int:pk>/detail", ReservationDetailView, name="detail"),
     path("<int:pk>/delete/", ReservationDeleteView, name="reservation_delete"),
+    path("<int:pk>/update_date/", ReservationDateUpdateView, name="update_date"),
+    path("<int:pk>/pick_date/", ReservationPickDateView, name="date_pick"),
+    path("<int:pk>/update_time/", ReservationTimeUpdateView, name="update_time"),
+    path("<int:pk>/pick_time/", ReservationPickTimeView, name="time_pick"),
+
    ]
--- reservations/views.py
+++ reservations/views.py
@@ -25,7 +25,7 @@ def ReservationCreateView(request):
             plan.save()
             form.save_m2m()
             print(request.POST)
-            return redirect("datetime", pk=plan.pk)
+            return redirect("date_pick", pk=plan.pk)
     else:
         form = ReservationForm()
     context = {"form": form}
@@ -34,46 +34,6 @@ def ReservationCreateView(request):
         "reservations/create.html",
         context,
     )
-
-@login_required
-def ReservationPickDateTimeView(request, pk):
-    date_list = pd.date_range(start=datetime.today().strftime('%Y-%m-%d'),end=(datetime.today() + timedelta(days=7)),freq="D").to_pydatetime().tolist()
-    filtered_list = []
-    for item in Reservation.objects.values_list('service_date_time').all():
-        if item[0]:
-            year = item[0].year
-            month = item[0].month
-            day = item[0].day
-            hour = item[0].hour
-            filtered_list.append(datetime(year,month,day,hour,0))
-    new_list = []
-    for date in date_list:
-        year = date.year
-        month = date.month
-        day = date.day
-        for hour in range(9,19):
-            if datetime(year,month,day,hour,0) not in filtered_list:
-                new_list.append(datetime(year,month,day,hour,0))
-    if request.method == "GET":
-        print(request.GET)
-    context = {"reservation": Reservation.objects.filter(user=request.user).get(pk=pk), "times": new_list}
-
-    return render(request, "reservations/datetime.html", context)
-
-@login_required
-def ReservationUpdateView(request, pk):
-    plan = Reservation.objects.filter(user=request.user).get(pk=pk)
-    if request.method == "POST":
-        str_date = request.POST["date"]
-        list_date = str_date.split(",")
-        month = int(list_date[1])
-        plan.service_date_time = datetime(int(list_date[0]),month,int(list_date[2]),int(list_date[3]),0)
-        plan.save()
-
-    context = {"reservations": Reservation.objects.filter(user=request.user)}
-
-    return render(request, "reservations/home.html", context)
-
 
 
 @login_required
@@ -98,3 +58,83 @@ def ReservationDeleteView(request, pk):
         "reservation": plan,
     }
     return render(request, "reservations/delete.html", context)
+
+@login_required
+def ReservationDateUpdateView(request, pk):
+    plan = Reservation.objects.filter(user=request.user).get(pk=pk)
+    if request.method == "POST":
+        str_date = request.POST["date"]
+        list_date = str_date.split(",")
+        month = int(list_date[1])
+        plan.service_date_time = datetime(int(list_date[0]),month,int(list_date[2]),0,0)
+        plan.save()
+        return redirect("time_pick", pk=pk)
+
+    context = {"reservations": Reservation.objects.filter(user=request.user)}
+
+    return render(request, "reservations/datepick.html", context)
+
+@login_required
+def ReservationPickDateView(request, pk):
+    date_list = pd.date_range(start=datetime.today().strftime('%Y-%m-%d'),end=(datetime.today() + timedelta(days=7)),freq="D").to_pydatetime().tolist()
+    filtered_list = []
+    for item in Reservation.objects.values_list('service_date_time').all():
+        if item[0]:
+            year = item[0].year
+            month = item[0].month
+            day = item[0].day
+            hour = item[0].hour
+            filtered_list.append(datetime(year,month,day,hour,0))
+    days_list = []
+    for date in date_list:
+        year = date.year
+        month = date.month
+        day = date.day
+        days_list.append(datetime(year,month,day,10,0))
+    if request.method == "GET":
+        print(request.GET)
+    context = {"reservation": Reservation.objects.filter(user=request.user).get(pk=pk), "dates": days_list}
+
+    return render(request, "reservations/datepick.html", context)
+
+
+@login_required
+def ReservationTimeUpdateView(request, pk):
+    plan = Reservation.objects.filter(user=request.user).get(pk=pk)
+    if request.method == "POST":
+        str_date = request.POST["time"]
+        list_date = str_date.split(",")
+        year = plan.service_date_time.year
+        month = plan.service_date_time.month
+        day = plan.service_date_time.day
+        plan.service_date_time = datetime(year,month,day,int(list_date[3]),0)
+        plan.save()
+
+    context = {"reservations": Reservation.objects.filter(user=request.user)}
+
+    return render(request, "reservations/home.html", context)
+
+@login_required
+def ReservationPickTimeView(request, pk):
+    plan = Reservation.objects.filter(user=request.user).get(pk=pk)
+    filtered_list = []
+    for item in Reservation.objects.values_list('service_date_time').all():
+        if item[0]:
+            year = item[0].year
+            month = item[0].month
+            day = item[0].day
+            hour = item[0].hour
+            filtered_list.append(datetime(year,month,day,hour,0))
+    time_list = []
+    exist_year = plan.service_date_time.year
+    exist_month = plan.service_date_time.month
+    exist_day = plan.service_date_time.day
+    for hour in range(9,19):
+        if datetime(exist_year,exist_month,exist_day,hour,0) not in filtered_list:
+            time_list.append(datetime(year,month,day,hour,0))
+    if request.method == "GET":
+        print(request.GET)
+
+    context = {"reservation": Reservation.objects.filter(user=request.user).get(pk=plan.pk), "times": time_list}
+
+    return render(request, "reservations/timepick.html", context)
