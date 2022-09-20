3d8f21c1a0b04a03ad26f59cf47be72155fc75d4
alper ademoglu <alperademoglu@hotmail.com>
2022-09-20 19:37:09 -0400

    Making changes for Heroku

--- cleaning_app/settings.py
+++ cleaning_app/settings.py
@@ -116,6 +116,9 @@ USE_I18N = True
 
 USE_TZ = True
 
+CSRF_TRUSTED_ORIGINS = [
+    'https://hemant-to-do.herokuapp.com'
+]
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
